8e534b0fa5046ac94c7ea0842634c397c66552e6
arthur <linchen00@foxmail.com>
2025-05-21 15:23:59 +0800

    chore: 添加pinia-plugin-persistdstate用于持久化状态管理

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
