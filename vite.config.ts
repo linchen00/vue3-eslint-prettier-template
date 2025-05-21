8618afefe8fb5614da7da8d237944f2d1fac93dd
arthur <linchen00@foxmail.com>
2025-05-21 16:49:13 +0800

    chore: 更新ESLint自动导入配置并添加vueuse组件

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VueUseComponentsResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -17,7 +19,11 @@ export default defineConfig({
 				enabled: true,
 				globalsPropValue: true
 			},
-			imports: ['vue', 'vue-router', 'pinia', 'vue-i18n']
+			imports: ['vue', 'vue-router', 'pinia', 'vue-i18n', '@vueuse/core']
+		}),
+		Components({
+			dts: true,
+			resolvers: [VueUseComponentsResolver()]
 		})
 	],
 	resolve: {
